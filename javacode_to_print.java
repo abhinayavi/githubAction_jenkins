b8f5dbe7b27aad3628363ef3350917fac2b44dc8
Abhinay <avi@example.com>
2024-08-17 18:30:58 +0000

    java code file added

---
+++ javacode_to_print.java
@@ -0,0 +1,12 @@
+public class PrintInfo {
+    public static void main(String[] args) {
+        System.out.println("Owner: Abhinay Kumar Sahu");
+        System.out.println("Repo url: https://github.com/abhinayavi/githubAction_jenkins.git");
+        System.out.println("This Project Tasks are:");
+        System.out.println("1. Create a code repository on GitHub");
+        System.out.println("2. Create a GitHub Action pipeline to perform continuous integration and (unit testing)");
+        System.out.println("3. Configure Tomcat Apache for automated code deployment which is configured in Jenkins server which will deploy the java project on Tomcat server");
+        System.out.println("4. Integrate the GitHub Action pipeline to invoke the Jenkins pipeline");
+        System.out.println("5. Invoke pipeline to validate automated deployment");
+    }
+}
